e975de6b32f887e17496602110bd9272da6098a5
Biplav Dahal <57222276+teamrdx@users.noreply.github.com>
2020-07-26 14:04:42 +0200

    Update Python_Assignment_1.py

--- Assignments/Python_Assignment_1.py
+++ Assignments/Python_Assignment_1.py
@@ -1,4 +1,4 @@
-#Write a program which will find all such numbers which are divisible by 7 but are not a #multiple of 5, between 2000 and 3200 (both included). The numbers obtained should be #printed in a comma-separated sequence on a single line.
+#Write a program which will find all such numbers which are divisible by 7 but are not a #multiple of 5, between 2000 and 3200 (both included). The numbers obtained should be printed in a comma-separated sequence on a single line.
 
 nums=[]
 for num in range(2000, 3201):
